c7bf26d03ada37e4ed46b6b4f03abaffe9cc00d3
Douglas <doug.neo@gmail.com>
2020-12-01 17:41:40 -0300

    Projeto iniciado

---
+++ S9V123/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace S9V123
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
