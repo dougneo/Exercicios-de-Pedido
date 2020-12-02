13b91510acb034ebe8c5e4017536e8a463b5b504
Douglas <doug.neo@gmail.com>
2020-12-01 23:03:10 -0300

    Classe Product modificada

--- S9V123/Entities/Product.cs
+++ S9V123/Entities/Product.cs
@@ -2,5 +2,18 @@
 {
     class Product
     {
+    	public string Name { get; set; }
+    	public double Price { get; set; }
+    	
+    	
+    	public Product()
+    	{
+    	}
+    	
+    	public Product(string name,  double price)
+    	{
+    		Name=name;
+    		Price=price;
+    	}
     }
 }
