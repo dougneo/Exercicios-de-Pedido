0a2a02fe86d1eb0308d46cb0546c0224e1c0e8aa
Douglas <doug.neo@gmail.com>
2020-12-02 01:31:29 -0300

    Varias acertos

--- S9V123/Entities/Enum/OrderStatus.cs
+++ S9V123/Entities/Enum/OrderStatus.cs
@@ -1,6 +1,10 @@
 ﻿namespace S9V123.Entities.Enum
 {
-    enum OrderStatus
+    enum OrderStatus : int
     {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
     }
 }
---
+++ S9V123/Entities/Order.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace S9V123.Entities
+{
+    class Order
+    {
+    }
+}
