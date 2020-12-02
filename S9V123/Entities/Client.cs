b5d23396b2604b97bfe3dc123cdf80a57c1d2a65
Douglas <doug.neo@gmail.com>
2020-12-02 01:23:30 -0300

    Atualização da classe 'Client'

--- S9V123/Entities/Client.cs
+++ S9V123/Entities/Client.cs
@@ -1,20 +1,22 @@
-﻿using System
+﻿using System;
 
 namespace S9V123.Entities
 {
     class Client
     {
-    	public string Name { get; set; }
-    	public string Email { get; set; }
-    	public DateTime BirthDate { get; set; }
-    	
-    	public Client(){
-    }
-    	public Client(string name, string email, DateTime birthdate){
-    		Name=name;
-    		Email=email;
-    		BirthDate=birthdate;
-    	}
-    	
-    	
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public Client()
+        {
+        }
+        public Client(string name, string email, DateTime birthdate)
+        {
+            Name = name;
+            Email = email;
+            BirthDate = birthdate;
+        }
+
+    }	
 }
