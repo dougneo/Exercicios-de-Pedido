876e6ae095bc80cfb4997d04d8e1d0e7b7e8930c
Douglas <doug.neo@gmail.com>
2020-12-01 22:49:13 -0300

    Classe Client modificada

--- S9V123/Entities/Client.cs
+++ S9V123/Entities/Client.cs
@@ -1,6 +1,20 @@
-﻿namespace S9V123.Entities
+﻿using System
+
+namespace S9V123.Entities
 {
     class Client
     {
+    	public string Name { get; set; }
+    	public string Email { get; set; }
+    	public DateTime BirthDate { get; set; }
+    	
+    	public Client(){
     }
+    	public Client(string name, string email, DateTime birthdate){
+    		Name=name;
+    		Email=email;
+    		BirthDate=birthdate;
+    	}
+    	
+    	
 }
